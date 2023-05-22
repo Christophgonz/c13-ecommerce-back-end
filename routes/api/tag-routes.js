4d9530e6f8e26ce5e343a100e1c2a8bedf491180
Christopher <christoph.gonz@gmail.com>
2023-05-22 16:59:19 -0700

    added put route for tag

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -41,6 +41,47 @@ router.post("/", (req, res) => {
 
 router.put("/:id", (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update(req.body, {
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((tag) => {
+      if (req.body.productIds && req.body.productIds.length) {
+        ProductTag.findAll({
+          where: { tag_id: req.params.id },
+        }).then((productTags) => {
+          // create filtered list of new tag_ids
+          const productTagIds = productTags.map(({ product_id }) => product_id);
+          const newProductTags = req.body.tagIds
+            .filter((product_id) => !productTagIds.includes(product_id))
+            .map((product_id) => {
+              return {
+                product_id: req.params.id,
+                product_id,
+              };
+            });
+
+          // figure out which ones to remove
+          const productTagsToRemove = productTags
+            .filter(
+              ({ product_id }) => !req.body.productIds.includes(product_id)
+            )
+            .map(({ id }) => id);
+          // run both actions
+          return Promise.all([
+            ProductTag.destroy({ where: { id: productTagsToRemove } }),
+            ProductTag.bulkCreate(newProductTags),
+          ]);
+        });
+      }
+
+      return res.json(tag);
+    })
+    .catch((err) => {
+      // console.log(err);
+      res.status(400).json(err);
+    });
 });
 
 router.delete("/:id", async (req, res) => {
